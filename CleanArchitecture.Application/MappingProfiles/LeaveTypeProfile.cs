1addbceaa8faf03437706fe6ec501cf9f8bf7798
Phúcc <lamminhphuc285@gmail.com>
2023-10-30 22:03:20 +0700

    Add project files.

---
+++ CleanArchitecture.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using CleanArchitecture.Domain;
+
+namespace CleanArchitecture.Application.MappingProfiles
+{
+    public class LeaveTypeProfile : Profile
+    {
+        public LeaveTypeProfile() 
+        { 
+           /* CreateMap<*//*LeaveTypeDTO,*//*LeaveType>().ReverseMap();*/
+        }
+    }
+}
